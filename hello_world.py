a907756f86c9d4e4a8a5a111c20eef00d2da8d88
Joaquin Trujillo <joaquin.trujillo@workday.com>
2015-04-02 19:58:47 -0700

    adding the hello world file; modified

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,21 @@
 print "hello world"
+
+phrase = "hello world"
+
+print phrase
+
+print 1 + 2
+
+print 1 * 2
+
+print 1/2
+
+print 1-2
+
+print 2**2
+
+print 2**3
+
+phrase2 = "hello again!"
+
+print phrase + phrase2
